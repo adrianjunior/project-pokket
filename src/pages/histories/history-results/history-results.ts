d7c6ea359f0bc2a4cc8cbe1f0943eea77464de86
aluno <aluno>
2018-10-08 17:57:38 -0300

    aa

--- src/pages/histories/history-results/history-results.ts
+++ src/pages/histories/history-results/history-results.ts
@@ -24,9 +24,12 @@ export class HistoryResultsPage implements OnInit {
   historyId: number;
   history: History;
   diagnostics: Diagnostic[];
-  diagnosticsIds: number[]
+  diagnosticsIds: number[];
+  diagnosticsItemsIds;
+  diagnosticsItems;
 
   section: number = 0;
+  hasData: boolean = false;
 
   constructor(public navCtrl: NavController, private storage: Storage,
     private navParams: NavParams, public appCtrl: App, public toastCtrl: ToastController,
@@ -42,12 +45,120 @@ export class HistoryResultsPage implements OnInit {
     }
     this.diagnostics = [];
     this.diagnosticsIds = [];
+    this.diagnosticsItemsIds = [];
+    this.diagnosticsItems = [];
     this.historyId = this.navParams.get('id');
     this.getHistory(this.historyId);
   }
 
-  onSelectSection(){ 
-    
+  onSelectSection() {
+
+  }
+
+  getSectionData(items: Item[][], section: number) {
+    const dataset: any[][] = [];
+    const incomes: number[] = [];
+    const expenses: number[] = [];
+    const fixedCompulsory: number[] = [];
+    const fixedOptional: number[] = [];
+    const variableCompulsory: number[] = [];
+    const variableOptional: number[] = [];
+    const dates: string[] = [];
+    this.diagnosticsIds.forEach((id, i) => {
+      let income = 0;
+      let expense = 0;
+      let fce = 0;
+      let foe = 0;
+      let vce = 0;
+      let voe = 0;
+      items[i].forEach((item, j) => {
+        if (item.value > 0) {
+          income += item.value;
+        } else {
+          expense += item.value;
+          if (item.type == 1) {
+            fce += item.value;
+          } else if (item.type == 2) {
+            foe += item.value;
+          } else if (item.type == 3) {
+            vce += item.value;
+          } else if (item.type == 4) {
+            voe += item.value;
+          }
+        }
+        console.log()
+        if (j >= items[i].length - 1) {
+          incomes.push(income);
+          expenses.push(expense);
+          fixedCompulsory.push(fce);
+          fixedOptional.push(foe);
+          variableCompulsory.push(vce);
+          variableOptional.push(voe);
+        }
+      })
+      dates.push(this.diagnostics[i].date)
+    })
+    if (section == 0) {
+      dataset[0] = ['Receitas', ...incomes];
+      dataset[1] = ['Despesas', ...expenses];
+    } else if (section == 1) {
+      dataset[0] = ['Fixo Obrigatório', ...fixedCompulsory];
+      dataset[1] = ['Fixo Opcional', ...fixedOptional];
+      dataset[2] = ['Variável Obrigatório', ...variableCompulsory];
+      dataset[3] = ['Variável Opcional', ...variableOptional];
+    }
+    this.createChart(dataset, dates);
+  }
+
+  createChart(dataset: any[][], dates: string[]) {
+    Chart.defaults.global.legend.position = 'top';
+    this.chartEl = new Chart(this.chart.nativeElement, {
+      type: this.chartType,
+      data: {
+        labels: dates,
+        datasets: [],
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: {
+          duration: 1000
+        },
+        legend: {
+          labels: {
+            fontFamily: 'Roboto',
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+    console.log('DATASET: ' + dataset)
+    dataset.forEach((labelDataset, index) => {
+      console.log('TOTAL: ' + labelDataset)
+      console.log('LABEL: ' + labelDataset[0])
+      console.log('DATA: ' + labelDataset.shift())
+      if (labelDataset.length != 0) {
+        this.chartEl.data.datasets.push({
+          label: labelDataset[0],
+          data: labelDataset.shift(),
+          duration: 500,
+          easing: 'easeInQuart',
+          fill: false,
+          borderColor: colors[index],
+          borderWidth: 3,
+          backgroundColor: colors[index],
+          lineTension: 0,
+          pointRadius: 3
+        })
+        this.chartEl.update();
+      }
+    })
   }
 
   presentToast(message: string, position: string, duration: number) {
@@ -70,55 +181,68 @@ export class HistoryResultsPage implements OnInit {
         }
         this.history = val;
       })
-      /*.then(() => {
-        this.getDiagnosticsItems();
+      .then(() => {
+        this.getDiagnosticsItems(this.history);
+      })
+      .then(() => {
+        this.getDiagnostics(this.history);
       })
       .catch(err => {
         this.presentToast('Ocorreu um erro ao carregar seu Resultado. Por favor, reinicie o app.', 'bottom', 3000);
         console.log(err);
-      })*/
+      })
   }
 
-  /*getDiagnosticsItems(history: History) {
-    history.diagnosticsIds.forEach(diagnosticId => {
-      
+  getDiagnosticsItems(history: History) {
+    this.diagnosticsIds = history.diagnosticsIds;
+    history.diagnosticsIds.forEach((diagnosticIds, i) => {
+      this.diagnosticsItemsIds[i] = []
+      this.diagnosticsItems[i] = []
     })
-    this.storage.get(`Diagnostic ${diagnosticId} Items`)
-      .then(val => {
-        if (val != null) {
-          this.itemsIds = val
-          if (this.itemsIds.length <= 0) {
+    history.diagnosticsIds.forEach((diagnosticId, i) => {
+      this.storage.get(`Diagnostic ${diagnosticId} Items`)
+        .then(val => {
+          if (val != null) {
+            val.forEach(id => {
+              this.diagnosticsItemsIds[i].push(id);
+            })
+          } else {
             this.hasData = false;
           }
-        } else {
-          this.hasData = false;
-        }
-        this.itemsIds.forEach(id => {
-          this.getDiagnosticItem(diagnosticId, id, isTypeData, type)
+          this.diagnosticsItemsIds[i].forEach(id => {
+            this.getDiagnosticItem(diagnosticId, id, this.section, i)
+          })
         })
-      })
-      .catch(err => {
-        this.presentToast('Ocorreu um erro ao carregar seu Resultado. Por favor, reinicie o app.', 'bottom', 3000);
-        console.log(err);
-      })
+        .catch(err => {
+          this.presentToast('Ocorreu um erro ao carregar seu Resultado. Por favor, reinicie o app.', 'bottom', 3000);
+          console.log(err);
+        })
+    })
   }
 
-  getDiagnosticItem(diagnosticId: number, itemId: number, isTypeData: boolean, type: number) {
+  getDiagnosticItem(diagnosticId: number, itemId: number, section: number, diagnosticIndex: number) {
     this.hasData = true;
     this.storage.get(`Diagnostic ${diagnosticId} Item ${itemId}`)
       .then(val => {
         if (val != null) {
-          this.items.push(val);
+          this.diagnosticsItems[diagnosticIndex].push(val);
         }
-        console.log(`ITEMS ${this.items.length} X ${this.itemsIds.length} IDS`)
-        if (this.items.length >= this.itemsIds.length) {
-
-          if (isTypeData) {
-            this.getTypeData(this.items, type)
-          } else {
-            this.getItemData(this.items, type)
+        if (diagnosticIndex >= this.diagnosticsIds.length - 1) {
+          if (this.diagnosticsItems[diagnosticIndex].length >= this.diagnosticsItemsIds[diagnosticIndex].length) {
+            this.getSectionData(this.diagnosticsItems, section)
           }
         }
       })
-  }*/
+  }
+
+  getDiagnostics(history: History) {
+    history.diagnosticsIds.forEach((diagnosticId, i) => {
+      this.storage.get(`Diagnostic ${diagnosticId}`)
+        .then(val => {
+          if (val != null) {
+            this.diagnostics.push(val);
+          }
+        })
+    })
+  }
 }
