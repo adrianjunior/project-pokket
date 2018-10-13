9b57317582ac2f297a79acfdbae7e43d6e794404
Adrian Junior <dev.adrianjunior@gmail.com>
2018-10-13 16:29:56 -0300

    quase add carteira ao diagnostico

--- src/pages/diagnostics/add-diagnostic-wallet/add-diagnostic-wallet.ts
+++ src/pages/diagnostics/add-diagnostic-wallet/add-diagnostic-wallet.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-import moment from 'moment';
 
 import { Wallet } from '../../../models/wallet.interface';
-import { Transaction } from '../../../models/transaction.interface';
-import { Diagnostic } from '../../../models/diagnostic.interface';
 import { Item } from '../../../models/item.inferface';
+import { Diagnostic } from '../../../models/diagnostic.interface';
 
 @IonicPage()
 @Component({
@@ -16,28 +13,35 @@ import { Item } from '../../../models/item.inferface';
 })
 export class AddDiagnosticWalletPage {
 
-  selectedWallets: boolean[];
-  selected: number;
   walletsIds: number[];
   wallets: Wallet[];
+  selected: number;
+  walletsSelector: boolean[];
+  selectedWallets: number[];
   transactionsIds: number[];
   itemNextId: number;
   diagnosticId: number;
   itemsIds: number[];
-  newIdList: number[];
+  diagnostic: Diagnostic;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    private storage: Storage, public formBuilder: FormBuilder,
-    private toastCtrl: ToastController) { }
+    private storage: Storage, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.itemNextId = 0;
-    this.walletsIds = [];
     this.wallets = [];
+    this.walletsIds = [];
+    this.walletsSelector = [];
     this.selectedWallets = [];
-    this.newIdList = [];
     this.selected = 0;
+    this.diagnostic = {
+      id: 0,
+      name: '',
+      date: '',
+      isConcluded: false
+    }
     this.diagnosticId = this.navParams.get('id');
+    this.getDiagnostic(this.diagnosticId);
     this.getWalletsIds();
     this.getItemsIds(this.diagnosticId);
     this.getNextItemId(this.diagnosticId);
@@ -45,17 +49,24 @@ export class AddDiagnosticWalletPage {
 
   updateSelected() {
     this.selected = 0;
-    this.selectedWallets.forEach(d => {
+    
+    this.wallets.forEach((d, i) => {
       if(d) {
         this.selected += 1;
+        this.selectedWallets.push(this.wallets[i].id);
+        console.log(`SELECTED: ${this.selected} / SELECTED_ARRAY: ${this.selectedWallets}`)
       }
     });
   }
 
   addWalletsToDiagnostic() {
-    for(let i = 0; i < this.selected; i++) {
-      this.newIdList.push(this.itemNextId + i);
-    }
+    this.selectedWallets.forEach((walletId, index) => {
+      if(index >= this.selectedWallets.length-1) {
+        this.getWalletTransactionsIds(walletId, true);
+      } else {
+        this.getWalletTransactionsIds(walletId, false);
+      }
+    })
   }
 
   presentToast(message: string, position: string, duration: number) {
@@ -70,13 +81,32 @@ export class AddDiagnosticWalletPage {
 
   // DATABASE FUNCTIONS
 
+  getDiagnostic(diagnosticId: number) {
+    this.storage.get(`Diagnostic ${diagnosticId}`)
+      .then(val => {
+        this.diagnostic = val;
+      })
+  }
+
   getWalletsIds() {
     this.storage.get(`WalletsIds`)
       .then(val => {
         if (val != null) {
           this.walletsIds = val;
+          this.walletsIds.forEach(walletId => {
+            this.getWallet(walletId);
+          });
         }
       })
+  }
+
+  getWallet(walletId: number) {
+    this.storage.get(`Wallet ${walletId}`)
+      .then(val => {
+        if (val != null) {
+          this.wallets.push(val);
+        }
+      });
   }
 
   getItemsIds(diagnosticId: number) {
@@ -84,54 +114,70 @@ export class AddDiagnosticWalletPage {
       .then(val => {
         this.itemsIds = val;
       })
-  }
+    }
 
-  setItemsIds(diagnosticId: number) {
-    this.itemsIds.push(...this.newIdList);
-    this.storage.set(`Diagnostic ${this.diagnosticId} Items`, this.itemsIds)
-  }
-
-  addItem(item: Item) {
-    this.storage.set(`Diagnostic ${this.diagnosticId} Item ${item.id}`, item)
-      .then(() => {
-        this.setItemsIds(item.diagnostic);
-      })
-  }
-
-  getWalletTransactionsIds(walletId: number) {
+  getWalletTransactionsIds(walletId: number, isLast: boolean) {
+    let transactionsIds;
     this.storage.get(`Wallet ${walletId} Transactions`)
       .then(val => {
         if(val != null) {
-          this.transactionsIds = val;
+          transactionsIds = val;
         }
-        this.transactionsIds.forEach((id, index) => {
+        transactionsIds.forEach((id, index) => {
           // Recebe uma transação
-          this.getWalletTransaction(id, walletId, index);
+          if(index >= transactionsIds.length-1 && isLast) {
+            // É a última
+            this.getWalletTransaction(id, walletId, true);
+          } else {
+            // Não é a última
+            this.getWalletTransaction(id, walletId, false);
+          }
         })
       })
   }
 
-  getWalletTransaction(transactionId: number, walletId: number, index: number) {
+  getWalletTransaction(transactionId: number, walletId: number, isLast: boolean) {
     this.storage.get(`Wallet ${walletId} Transaction ${transactionId}`)
       .then(val => {
         console.log(val)
+        console.log(`ITEM NEXT ID: ${this.itemNextId}`)
         if(val != null) {
           let item: Item = {
-            id: this.newIdList[index],
+            id: this.itemNextId,
             name: val.name,
             value: val.value,
             diagnostic: this.diagnosticId,
             category: val.category,
             type: val.type
           }
-          this.addItem(item);
+          this.itemNextId += 1;
+          if(isLast) {
+            this.addItem(item, true);
+          } else {
+            this.addItem(item, false);
+          }
         }
       })
   }
 
+  addItem(item: Item, isLast: boolean) {
+    this.storage.set(`Diagnostic ${this.diagnosticId} Item ${item.id}`, item)
+      .then(() => {
+        this.itemsIds = [...this.itemsIds, item.id]
+        if (isLast) {
+          this.setItemsIds(this.diagnosticId);
+          this.setNextItemId(this.diagnosticId, this.itemsIds[this.itemsIds.length - 1])
+        }
+      })
+  }
+
+  setItemsIds(diagnosticId: number) {
+    this.storage.set(`Diagnostic ${diagnosticId} Items`, this.itemsIds)
+  }
+
   setNextItemId(diagnosticId: number, id: number) {
-    id += this.newIdList.length;
     this.storage.set(`Diagnostic ${diagnosticId} Next Item id`, id)
+    this.navCtrl.pop();
   }
 
   getNextItemId(diagnosticId: number) {
